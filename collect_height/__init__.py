195e2556ff4aae82d7302fb4d26f297135706044
tommy2tone <thomas.hildebrand1@gmail.com>
2019-11-22 09:57:36 -0500

    update database url

--- collect_height/__init__.py
+++ collect_height/__init__.py
@@ -1,9 +1,12 @@
+
+
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+
     with Configurator(settings=settings) as config:
         config.include('.models')
         config.include('pyramid_jinja2')
