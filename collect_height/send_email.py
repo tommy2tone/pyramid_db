4efd6814f7dad4dabfb4393a72ed7eceb0dea5f8
tommy2tone <thomas.hildebrand1@gmail.com>
2022-05-26 21:58:25 -0400

    Update send_email.py

--- collect_height/send_email.py
+++ collect_height/send_email.py
@@ -4,7 +4,7 @@ import smtplib
 
 def send_email(email, height, average_height, count):
     from_email = "thomas.hildebrand11@gmail.com"
-    from_pw = "@Testing1234"
+    from_pw = "ykgkkssfjwbsuafg"
     to_email = email
 
     subject = "Height Data"
